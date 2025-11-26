2905c6e531ea76f61faf6995ec69080f7f154860
Efrizali <148589971+Efrizali@users.noreply.github.com>
2025-11-25 22:34:17 -0700

    It works
    But I have notes on how I'd like to make it better

---
+++ Memorizor/Input.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorizor
+{
+    internal class Input
+    {
+        public static int GetMenuOption(int optionCount)
+        {
+            return GetSpecificIntFromUser(1, optionCount);
+        }
+
+        private static String GetStringFromUser()
+        {
+            return Console.ReadLine();
+        }
+
+        private static int GetIntFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Int: ");
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    return input;
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+            }
+        }
+
+        private static int GetSpecificIntFromUser(int min, int max)
+        { 
+            while (true)
+            {
+                Console.Write("Int: ");
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    if (input < min | input > max) continue;
+                    return input;
+                } catch (Exception e)
+                {
+                    continue;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Memorizor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memorizor
+{
+    internal class Program
+    {
+        private static readonly List<List<String>> Studyables = new List<List<String>>();
+
+        private static readonly Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            AddTerms();
+
+            Study();
+        }
+
+        // Next Steps:
+        // 
+        // Create a system for reading a file to get the info
+        //      Study groups (IEEE, Digital Signal, Ethernet Standards, etc)
+        //      Header(Group, Question, Answer)
+        //      Data
+        // Create a system to write the file
+
+        private static void Study()
+        {
+            Console.WriteLine("Begin!");
+            while (true)
+            {
+                List<String> term = Studyables[rnd.Next(Studyables.Count)];
+                Console.WriteLine($"\n{term[0]}\n");
+
+                Console.Write("Press Enter to reveal");
+                Console.ReadLine();
+
+                Console.WriteLine($"\n{term[1]}");
+
+                Console.Write("\n\nPress Enter For next Term");
+                Console.ReadLine();
+            }
+        }
+
+        private static void AddTerms()
+        {
+            Studyables.Add(new List<String> { "hi", "Meh" });
+            Studyables.Add(new List<String> { "bye", "tired" });
+        }
+    }
+}
